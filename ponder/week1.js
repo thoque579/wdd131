23f9128996d372c8039ab2457c2208d919332f1e
tusharhoque <tusharhoque2@gmail.com>
2026-09-19 10:57:27 -0600

    meta-css.html

---
+++ ponder/week1.js
@@ -0,0 +1,25 @@
+let num = 6;
+
+num = 10;
+
+const password = 'R3d1ng!';
+
+
+const pi =3.14;
+
+let radius = 5;
+
+const one = 1;
+const two = 2;
+
+let course = 'WDD 131';
+
+if(true) {
+    let student = 'John';
+    console.log(course);
+    console.log(student);
+
+}
+
+console.log(course);
+console.log(student);
