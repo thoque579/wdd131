00bebca036f93506e37e0f192c99c6a326741060
tusharhoque <tusharhoque2@gmail.com>
2026-09-21 20:26:17 -0600

    fix ponder assignment

--- ponder/week1.js
+++ ponder/week1.js
@@ -6,11 +6,26 @@ const password = 'R3d1ng!';
 
 
 const pi =3.14;
-
 let radius = 5;
+
+console.log("pi:" + pi);
+console.log("radius:" + radius);
+
+ radius = 6;
+
+console.log("new_radius:" + radius);
+
 
 const one = 1;
 const two = 2;
+
+console.log("one:" + one);
+console.log("two:" + two);
+
+console.log("one + two:" + (one + two));
+
+console.log(typeof one);
+console.log(typeof two);
 
 let course = 'WDD 131';
 
